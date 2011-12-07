408d54f2bc464505673a497c2f6acf3f4f7dc837
Josh Butts <josh@joshbutts.com>
2011-12-07 07:47:34 -0600

    version 1

---
+++ src/Spray/Response.php
@@ -0,0 +1,44 @@
+<?php
+namespace Spray;
+class Response
+{
+    const STATUS_200 = "200 OK";
+    const STATUS_404 = "404 Not Found";
+    const STATUS_301 = "301 Moved Permanently";
+    const STATUS_302 = "302 Moved";
+
+    protected $statusCode = self::STATUS_200;
+    protected $headers = array();
+    protected $body;
+
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+
+    public function setHeaders($headers)
+    {
+        $this->headers = $headers;
+    }
+
+    public function setHeader($headerName, $headerValue)
+    {
+        $this->headers[$headerName] = $headerValue;
+    }
+
+    public function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+    }
+
+    public function __toString()
+    {
+        $output = "HTTP/1.0 $this->statusCode\r\n";
+        foreach ($this->headers as $header => $value) {
+            $output .= "$header: $value\r\n";
+        }
+        $output .= "\r\n";
+        $output .= $this->body;
+        return $output;
+    }
+}
---
+++ src/Spray/Wrapper.php
@@ -0,0 +1,67 @@
+<?php
+namespace Spray;
+
+require_once __DIR__ . DIRECTORY_SEPARATOR  . 'Request.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR  . 'Response.php';
+
+class Wrapper
+{
+    protected $output = '';
+    protected $currentPosition = 0;
+
+    protected static $overrideWrappers = array('http', 'https');
+    protected static $response;
+    protected static $request;
+
+    public static function init()
+    {
+        $existingWrappers = stream_get_wrappers();
+        foreach (self::$overrideWrappers as $wrapper) {
+            if (in_array($wrapper, $existingWrappers)) {
+                stream_wrapper_unregister($wrapper);
+                stream_wrapper_register($wrapper, "Spray\\Wrapper", true);
+            }
+        }
+    }
+
+    public function stream_open($path, $mode, $options, &$opened_path)
+    {
+        self::$request = new Request();
+        $this->output = (string) self::$response;
+        return true;
+    }
+
+    public function stream_write($data)
+    {
+        echo $data;
+    }
+
+    public function stream_eof()
+    {
+        return $this->currentPosition >= strlen($this->output);
+    }
+
+    public function stream_read($count)
+    {
+        $val = substr($this->output, $this->currentPosition, $count);
+        $this->currentPosition += $count;
+        return $val;
+    }
+
+    public static function setResponse(Response $response)
+    {
+        self::$response = $response;
+    }
+
+    public function stream_stat()
+    {
+        return array();
+    }
+
+    public static function reset()
+    {
+        foreach (self::$overrideWrappers as $wrapper) {
+            stream_wrapper_restore($wrapper);
+        }
+    }
+}
