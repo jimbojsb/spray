bd3251d1a9035e5805745861f1fbef5b04053600
Scott Reis <scott.reis@vertive.com>
2012-02-01 16:28:14 -0600

    rewrite basic integration test for new functionality

--- tests/spray/IntegrationTest.php
+++ tests/spray/IntegrationTest.php
@@ -3,16 +3,13 @@ class IntegrationTest extends PHPUnit_Framework_TestCase
 {
     public function testSpray()
     {
-        Spray\Wrapper::init();
+        $response = array('status' => Spray::STATUS_200,
+                          'headers' => array("connection" => "close"),
+                          'body' => "{foo: bar}");
+        Spray::stub('http://test', $response);
 
-        $response = new Spray\Response();
-        $response->setStatusCode(Spray\Response::STATUS_302);
-        $response->setHeader('Location', 'http://www.bing.com/');
-        $response->setBody('thisisabody');
-        Spray\Wrapper::setResponse($response);
+        $expected = "HTTP/1.0 200 OK\r\nconnection: close\r\n\r\n{foo: bar}";
 
-        $expected = "HTTP/1.0 302 Moved\r\nLocation: http://www.bing.com/\r\n\r\nthisisabody";
-        $response = file_get_contents('http://www.google.com/');
-        $this->assertEquals($expected, $response);
+        $this->assertEquals($expected, file_get_contents('http://test'));
     }
 }
